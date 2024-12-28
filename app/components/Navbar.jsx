e3ba8baaee30e5324311e577736928146cd34fba
Vaideesh Deshmukh <vaideeshdeshmukh1608@gmail.com>
2024-12-28 15:00:49 +0530

    added navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+"use client"
 
-const Navbar = () => {
+import React from "react";
+import Link from "next/link";
+import { Menu, X } from "lucide-react";
+
+const menuItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
+
+export function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="w-full bg-orange-400 p-1 sticky top-0 z-50">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+        <div className="inline-flex items-center space-x-2">
+          <span>
+            <Link href="/">
+              <img
+                src="/logo.png"
+                className="w-[30px] h-[30px] rounded-full"
+                alt="logo"
+              />
+            </Link>
+          </span>
+          <Link href="/">
+            <span className="font-bold">EduTrack</span>
+          </Link>
+        </div>
+        <div className="hidden grow items-start lg:flex">
+          <ul className="ml-12 inline-flex space-x-8">
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <Link href={item.href}>
+                  <span className="inline-flex items-center text-sm font-semibold text-gray-800 hover:text-gray-900">
+                    {item.name}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="hidden space-x-2 lg:block">
+          <Link href="/signup">
+            <span className="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+              Sign Up
+            </span>
+          </Link>
+          <Link href="/login">
+            <span className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+              Log In
+            </span>
+          </Link>
+        </div>
+        <div className="lg:hidden">
+          <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
+        </div>
+        {isMenuOpen && (
+          <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pb-6 pt-5">
+                <div className="flex items-center justify-between">
+                  <div className="inline-flex items-center space-x-2">
+                    <div className="inline-flex items-center">
+                      <img
+                        src="/logo.png"
+                        width={30}
+                        height={30}
+                        alt="logo"
+                        className="rounded-full"
+                      />
+                    </div>
+                    <span className="font-bold">EduTrack</span>
+                  </div>
+                  <div className="-mr-2">
+                    <button
+                      type="button"
+                      onClick={toggleMenu}
+                      className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <X className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid gap-y-4">
+                    {menuItems.map((item) => (
+                      <Link key={item.name} href={item.href}>
+                        <span className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50">
+                          <span className="ml-3 text-base font-medium text-gray-900">
+                            {item.name}
+                          </span>
+                        </span>
+                      </Link>
+                    ))}
+                  </nav>
+                </div>
+                <div className="mt-4 space-y-2">
+                  <Link href="/login">
+                    <span className="mb-2 w-full rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                      Log In
+                    </span>
+                  </Link>
+                  <Link href="/signup">
+                    <span className="mx-3 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                      Sign Up
+                    </span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
