12c3ceccd463eabb27469e4e5ab53e153a20a652
ujjwalbokde <143083146+ujjwalbokde@users.noreply.github.com>
2024-12-28 14:56:14 +0530

    slider added

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
-import Events from "./components/Events";
 import ContactUs from "./components/ContactUs";
+import { Events } from "./components/Events";
+import { EventSlider } from "./components/slider/EventSlider";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <AboutUs/>
-      <Events/>
+      <EventSlider/>
       <ContactUs/>
     </>
   );
