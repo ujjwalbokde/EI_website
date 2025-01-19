f45ef335af2f987697275b45b6e631c2a3532c30
ujjwalbokde <143083146+ujjwalbokde@users.noreply.github.com>
2025-01-19 22:09:54 +0530

    done with landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
-// tailwind.config.js
-const {nextui} = require("@nextui-org/theme");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 };
